1626c9dd970ce38d508e7c45aa5104e614d7b044
Jeffrey Marshal Hays <j.hays.devs@gmail.com>
2025-08-16 07:21:27 -0400

    Implementing client-side validation on new issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,43 +1,68 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import { useForm, Controller } from 'react-hook-form'
 import axios from 'axios'
 import 'easymde/dist/easymde.min.css'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { InfoCircledIcon } from '@radix-ui/react-icons'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createIssueSchema } from '@/app/validationSchemas'
+import { z } from 'zod'
 
-interface IssueForm {
-	title: string
-	description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
 
 const NewIssuePage = () => {
 	const router = useRouter()
-	const { register, control, handleSubmit } = useForm<IssueForm>()
+	const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+		resolver: zodResolver(createIssueSchema)
+	})
+	const [error, setError] = useState('')
 
   return (
-    <form 
-			className='max-w-xl space-y-3'
-			onSubmit={handleSubmit(async (data) => {
-			axios.post('/api/issues', data).then(() => {
-				router.push('/issues')
-			})
-		})}>
-      <TextField.Root placeholder='Title' {...register('title')}>
-        <TextField.Slot />
-      </TextField.Root>
-			<Controller
-				control={control}
-				name='description'
-				render={({ field }) => (
-					<SimpleMDE placeholder='Description' {...field} />
-				)}
-			/>
-      <Button>Submit New Issue</Button>
-    </form>
+		<div className='max-w-xl'>
+			{error && 
+				<Callout.Root color='red' className='mb-5' variant='soft'>
+					<Callout.Icon>
+						<InfoCircledIcon />
+					</Callout.Icon>
+					<Callout.Text>
+						{error}
+					</Callout.Text>
+				</Callout.Root>
+			}
+			<form 
+				className='space-y-3'
+				onSubmit={handleSubmit(async (data) => {
+					console.log('data', data)
+					axios.post('/api/issues', data)
+						.then(() => {
+							router.push('/issues')
+						})
+						.catch(err => {
+							console.log('error', err)
+							setError(err.response.data)
+						})
+			})}>
+				<TextField.Root placeholder='Title' {...register('title')}>
+					<TextField.Slot />
+				</TextField.Root>
+				{errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+				<Controller
+					control={control}
+					name='description'
+					render={({ field }) => (
+						<SimpleMDE placeholder='Description' {...field} />
+					)}
+				/>
+				{errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+				<Button>Submit New Issue</Button>
+			</form>
+		</div>
   )
 }
 
