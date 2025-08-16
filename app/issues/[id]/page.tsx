d88b6c51f58524c84011135c8de636810f995beb
Jeffrey Marshal Hays <j.hays.devs@gmail.com>
2025-08-16 10:48:10 -0400

    Applying single responsibility principle to issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,13 +1,13 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import { prisma } from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import ReactMarkdown from 'react-markdown'
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 interface Props {
 	params: { id: string }
 }
 
-const DetailPage = async ({ params }: Props) => {
+const IssueDetailPage = async ({ params }: Props) => {
 	const issue = await prisma.issue.findUnique({
 		where: { id: parseInt(params.id) }
 	})
@@ -16,17 +16,15 @@ const DetailPage = async ({ params }: Props) => {
 		notFound()
 
 	return (
-		<div>
-			<Heading>{issue.title}</Heading>
-			<Flex gap='4' my='2'>
-				<IssueStatusBadge status={issue.status} />
-				<Text>{issue.createdAt.toLocaleDateString()}</Text>
-			</Flex>
-			<Card className='prose'>
-				<ReactMarkdown>{issue.description}</ReactMarkdown>
-			</Card>
-		</div>
+		<Grid columns={{ initial: '1', md: '2' }} gap='5'>
+			<Box>
+				<IssueDetails issue={issue} />
+			</Box>
+			<Box>
+				<EditIssueButton issueId={issue.id} />
+			</Box>
+		</Grid>
 	)
 }
 
-export default DetailPage
+export default IssueDetailPage
