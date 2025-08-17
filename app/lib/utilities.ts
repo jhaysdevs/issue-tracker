2ea5740032cb8c55f898c68379408c09ded2b88b
Jeffrey Marshal Hays <j.hays.devs@gmail.com>
2025-08-17 18:40:05 -0400

    Updating formatDate utility function

--- app/lib/utilities.ts
+++ app/lib/utilities.ts
@@ -1,12 +1,17 @@
-export const formatDate = (date: Date) => {
-	return new Intl.DateTimeFormat('en-US', {
-		year: 'numeric',
-		month: 'short',
+export const formatDate = (date: Date, shortened?: boolean) => {
+	const formattedDate = new Intl.DateTimeFormat('en-US', {
+		year: shortened ? '2-digit' : 'numeric',
+		month: shortened ? 'numeric' : 'short',
 		day: 'numeric',
-		hour: '2-digit',
+	}).format(date)
+	
+	const formattedTime = new Intl.DateTimeFormat('en-US', {
+		hour: 'numeric',
 		minute: '2-digit',
 		hour12: true,
 	}).format(date)
+	
+	return `${formattedDate}${shortened ? ' ' : ', '}${formattedTime}`
 }
 
 export const delay = (ms: number) =>
