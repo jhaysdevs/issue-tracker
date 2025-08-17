d907c3a820195f2e0f8f31a97a048f0f29dba03e
Jeffrey Marshal Hays <j.hays.devs@gmail.com>
2025-08-17 04:54:35 -0400

    Adding toast 🍞 notifications

--- app/issues/_components/AssigneeSelect.tsx
+++ app/issues/_components/AssigneeSelect.tsx
@@ -5,6 +5,7 @@ import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import { Skeleton } from '@/app/components'
 import { useEffect, useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 import axios from 'axios'
 
 export const GetUsers = () => {
@@ -34,30 +35,35 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 	if (error || !users) return null
 
 	return (
-		<Select.Root defaultValue={issue.assignedTo || ''} onValueChange={(userId) => {
-			axios.patch(`/api/issues/${issue.id}`, {
-				assignedTo: userId === '0' || null ? null : userId,
-			})
-		}}>
-			<Select.Trigger placeholder='Assign...' />
-			<Select.Content>
-				<Select.Group>
-					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value='0'>Unassigned</Select.Item>
-					{users?.map((user: User) => (
-						<Select.Item key={user.id} value={user.id}>
-							{user.name}
-						</Select.Item>
-					))}
-				</Select.Group>
-				<Select.Separator />
-				<Select.Group>
-					<Select.Label>Vegetables</Select.Label>
-					<Select.Item value='carrot'>Carrot</Select.Item>
-					<Select.Item value='potato'>Potato</Select.Item>
-				</Select.Group>
-			</Select.Content>
-		</Select.Root>
+		<>
+			<Select.Root defaultValue={issue.assignedTo || ''} onValueChange={(userId) => {
+				axios.patch(`/api/issues/${issue.id}`, {
+					assignedTo: userId === '0' || null ? null : userId,
+				}).catch((error) => {
+					toast.error('Error assigning issue')
+				})
+			}}>
+				<Select.Trigger placeholder='Assign...' />
+				<Select.Content>
+					<Select.Group>
+						<Select.Label>Suggestions</Select.Label>
+						<Select.Item value='0'>Unassigned</Select.Item>
+						{users?.map((user: User) => (
+							<Select.Item key={user.id} value={user.id}>
+								{user.name}
+							</Select.Item>
+						))}
+					</Select.Group>
+					<Select.Separator />
+					<Select.Group>
+						<Select.Label>Vegetables</Select.Label>
+						<Select.Item value='carrot'>Carrot</Select.Item>
+						<Select.Item value='potato'>Potato</Select.Item>
+					</Select.Group>
+				</Select.Content>
+			</Select.Root>
+			<Toaster />
+		</>
 	)
 }
 
