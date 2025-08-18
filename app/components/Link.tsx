dbd2204d4670995f59592d6ec0910f25e66d5f2a
Jeffrey Marshal Hays <j.hays.devs@gmail.com>
2025-08-17 21:28:22 -0400

    Refactoring TableCellLink and Link components to type split Radix props from Next.js props

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,16 +1,34 @@
-import NextLink from 'next/link'
+import NextLink, { type LinkProps as NextLinkProps } from 'next/link'
 import { Link as RadixLink } from '@radix-ui/themes'
-interface LinkProps {
+import * as React from 'react'
+
+// Extend both, but keep them separate
+interface LinkProps extends NextLinkProps, React.ComponentProps<typeof RadixLink> {
   href: string
-  className?: string
   children: React.ReactNode
-  style?: React.CSSProperties
 }
 
-const Link = ({ href, className, children, style }: LinkProps) => {
+const Link = ({ href, children, ...props }: LinkProps) => {
+  // Split Radix vs Next.js props
+  const {
+    // Radix props (color, underline, etc.)
+    color,
+    highContrast,
+    size,
+    underline,
+    weight,
+    ...nextProps
+  } = props
+
   return (
-    <RadixLink asChild className={className} style={style}>
-      <NextLink href={href}>
+    <RadixLink
+      asChild
+      color={color}
+      highContrast={highContrast}
+      size={size}
+      underline={underline}
+      weight={weight}>
+      <NextLink href={href} {...nextProps}>
         {children}
       </NextLink>
     </RadixLink>
--- app/components/TableCellLink.tsx
+++ app/components/TableCellLink.tsx
@@ -1,32 +1,29 @@
 import Link from './Link'
 import { Flex } from '@radix-ui/themes'
 
-interface TableCellLinkProps {
+interface TableCellLinkProps extends React.ComponentProps<typeof Link> {
   href: string
-	className?: string
   children: React.ReactNode
 }
 
-export const TableCellLink = ({ href, className, children }: TableCellLinkProps) => {
+export const TableCellLink = ({ href, children, ...props }: TableCellLinkProps) => {
   return (
-		<Flex
-			style={{
-				margin: 'calc(-1 * var(--table-cell-padding))',
-				width: 'calc(100% + var(--table-cell-padding) * 2)',
-				height: 'calc(100% + var(--table-cell-padding) * 2)',
-				alignItems: 'center',
-			}}
-		>
-			<Link
-				href={href}
-				className={className}
-				style={{
-					width: '100%',
-					padding: 'var(--table-cell-padding)'
-				}}
-			>
-				{children}
-			</Link>
-		</Flex>
+    <Flex
+      style={{
+        margin: 'calc(-1 * var(--table-cell-padding))',
+        width: 'calc(100% + var(--table-cell-padding) * 2)',
+        height: 'calc(100% + var(--table-cell-padding) * 2)',
+        alignItems: 'center',
+      }}>
+      <Link
+        href={href}
+        style={{
+          width: '100%',
+          padding: 'var(--table-cell-padding)',
+        }}
+        {...props}>
+        {children}
+      </Link>
+    </Flex>
   )
 }
