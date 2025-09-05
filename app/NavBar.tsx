42fecf47e4b543199f0e07de304aff7409bbe593
JF Hays <j.hays.devs@gmail.com>
2025-09-05 13:20:19 -0400

    Updating navbar link hover/active styles

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -59,7 +59,7 @@ const NavLinks = () => {
                 className={classnames(
                   'nav-link relative block select-none rounded-md px-3 py-2 text-base font-medium leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
                   {
-                    'bg-accent text-accent-foreground nav-link-active': link.href === currentPath,
+                    'nav-link-active': link.href === currentPath,
                   }
                 )}
                 style={{ margin: '0 -6px' }}
